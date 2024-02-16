f335a1275a525c5d1dddd8acaa5b745357216be6
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-02-17 00:45:57 +0100

    Removed docs

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,8 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://yoursite.qwik.dev",
+        // origin: "https://yoursite.qwik.dev",
+        origin: "https://gerardstelt.github.io/kros/"
       }),
     ],
   };
