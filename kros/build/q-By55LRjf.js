844484c8b502b15637ade45fe9019b2903813603
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-02-17 13:38:52 +0100

    Updates

---
+++ kros/build/q-By55LRjf.js
@@ -0,0 +1,1 @@
+import{P as t}from"./q-DoTjXb0A.js";const o=()=>t("div",null,{class:"container m-auto px-8 py-12"},"Contact",3,"YA_0");export{o as s_M0MhZOWBKK8};
---
+++ kros/build/q-UJxE5Kx2.js
@@ -0,0 +1,7 @@
+import{n as e,q as t,_ as i}from"./q-DoTjXb0A.js";const s=e(t(()=>i(()=>import("./q-By55LRjf.js"),__vite__mapDeps([])),"s_M0MhZOWBKK8")),n={title:"Contact | Kros heiwerk service",meta:[{name:"description",content:"Qwik site description"}]};export{s as default,n as head};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
