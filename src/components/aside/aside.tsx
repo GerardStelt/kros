f5ab9548055296b4de8eb4f88c373964367d4377
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-02-24 19:45:41 +0100

    Better offerte overlay without glitching

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -6,13 +6,13 @@ export default component$(() => {
     const open = useSignal(false);
 
     return (
-        <aside class={{ "w-screen h-screen fixed flex flex-col -translate-y-full top-[33px] transition-transform duration-300": true, "!top-0 !translate-y-0 bg-white": open.value }}>
-            <div class="grow flex justify-center items-center">Offerte formulier</div>
+        <aside class={{ "w-screen h-screen fixed flex flex-col -translate-y-full top-[33px] transition-transform duration-500": true, "!top-0 !translate-y-0": open.value }}>
+            <div class="grow flex justify-center items-center bg-white">Offerte formulier</div>
             <div class="text-center">
                 <button
                     onClick$={() => open.value = !open.value}
                     type="button"
-                    class={{ "rounded-b-2xl bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-tight text-white": true, "!rounded-none !rounded-t-2xl": open.value }}>
+                    class="rounded-b-2xl bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-tight text-white">
                     Offerte
                 </button>
             </div>
