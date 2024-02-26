fdc50e57b8475c227ca30f8a885454b2addaa468
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-02-27 00:46:33 +0100

    Updates

--- build/q-DwesxUiq.js
+++
@@ -1,7 +0,0 @@
-import{$ as l,c as s,P as t,M as r,q as o,_ as a}from"./q-BvEco60E.js";const u=()=>{const[e]=l();return e.value=!e.value},i=()=>{const e=s(!1);return t("aside",null,{class:r(n=>({"w-screen h-screen fixed flex flex-col -translate-y-full top-[33px] transition-transform duration-500":!0,"!top-0 !translate-y-0":n.value}),[e])},[t("div",null,{class:"grow flex justify-center items-center bg-white"},"Offerte formulier",3,null),t("div",null,{class:"text-center"},t("button",null,{class:"rounded-b-2xl bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-tight text-white",onClick$:o(()=>a(()=>Promise.resolve().then(()=>c),void 0),"s_5SAwNzhYoDY",[e]),type:"button"},"Offerte",3,null),3,null)],3,"1d_0")},c=Object.freeze(Object.defineProperty({__proto__:null,s_5SAwNzhYoDY:u,s_hS4qPk5ncn0:i},Symbol.toStringTag,{value:"Module"}));export{u as s_5SAwNzhYoDY,i as s_hS4qPk5ncn0};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
