ec12d9186f54deb71a1b6be659f0b2a350df3d52
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-02-27 23:58:08 +0100

    Updates

--- build/q-CAArmQ1R.js
+++
@@ -1,7 +0,0 @@
-import{n as e,q as o,_ as s,E as n,P as l,h as t,H as _}from"./q-BvEco60E.js";const r=e(o(()=>s(()=>import("./q-D3u8Falo.js"),__vite__mapDeps([])),"s_3lb9EjxI5sA")),a=e(o(()=>s(()=>import("./q-n1ywwps0.js"),__vite__mapDeps([])),"s_ceU05TscGYE")),u=()=>n(t,{children:[l("a",null,{class:"fixed top-0 left-1/2 bg-orange-500 px-4 p-2 rounded-b-xl text-white leading-none font-semibold",href:"/contact"},"offerte",3,null),n(a,null,3,"XF_0"),l("main",null,null,n(_,null,3,"XF_1"),1,null),n(r,null,3,"XF_2")]},1,"XF_3");export{u as s_VkLNXphUh5s};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
