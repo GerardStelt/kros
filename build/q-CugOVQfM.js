d8a1c0ff9088cd96fc8dc9e1b84071a35d79186d
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2024-03-04 10:12:09 +0100

    Updates

---
+++ build/q-CugOVQfM.js
@@ -0,0 +1,7 @@
+import{Q as e,S as r,G as t}from"./q-FjI7C5Fx.js";import{n as u,q as a,_ as s,E as l,P as n}from"./q-BvEco60E.js";const o=u(a(()=>s(()=>import("./q-B9UjsCw0.js"),__vite__mapDeps([])),"s_I0bU2he6ktg")),U=()=>l(e,{children:[n("head",null,null,[n("meta",null,{charSet:"utf-8"},null,3,null),n("link",null,{href:"/manifest.json",rel:"manifest"},null,3,null),l(o,null,3,"UU_0"),l(r,null,3,"UU_1")],1,null),n("body",null,{lang:"en"},l(t,null,3,"UU_2"),1,null)]},1,"UU_3");export{U as s_anRnQ0rGPN0};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- build/q-CwF8ce2H.js
+++
@@ -1,7 +0,0 @@
-import{n as o,q as t,_ as r}from"./q-BvEco60E.js";const a=()=>{throw"Symbol removed by Qwik Optimizer, it can not be called from current platform"},s=o(t(()=>r(()=>import("./q-w4HiHcZ-.js"),__vite__mapDeps([])),"s_2s0A5Q5Ek6o"));export{s as default,a as onGet};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
